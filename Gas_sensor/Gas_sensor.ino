123cc1cf6d3e32bfc3a33ac861c33468871708ad
KaloyanKutiyski <kaloyankutiysky@gmail.com>
2021-06-22 19:25:57 +0300

    fixed blynk command entirely overriding sensor

--- Gas_sensor/Gas_sensor.ino
+++ Gas_sensor/Gas_sensor.ino
@@ -4,12 +4,12 @@
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
-char auth[] = "auth_key";
+char auth[] = "";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "wifi_name";
-char pass[] = "wifi_pass";
+char ssid[] = "";
+char pass[] = "";
 
 #define MQ2A 34
 #define RELAY 26
@@ -29,17 +29,15 @@ void setup()
  pinMode(RELAY, OUTPUT);
  Blynk.begin(auth, ssid, pass);
  timer.setInterval(500L, readSensor); // read sensors every 500ms
- timer.setInterval(500L, readRemoteInput); // read data from mobile device every 500ms
+ timer.setInterval(100L, readRemoteInput); // read data from mobile device every 100ms
 }
 
 void readRemoteInput()
 {
   boolean isPressed = (digitalRead(BTN_PIN) == LOW);
   if (!isPressed) {
-    digitalWrite(RELAY, HIGH);
+    Serial.println("muted");
     last_stop = millis();
-  } else {
-    digitalWrite(RELAY, LOW);
   }
 }
 
@@ -50,10 +48,13 @@ void readSensor()
  
   if (sensorValueA > dangerLevel)
   {
-    if(millis() - last_stop < 10000)
+    if(millis() - last_stop > 10000)
     {
       digitalWrite(RELAY, LOW);
       Serial.println("Current Flowing");
+    } else {
+      Serial.println("muted, time remaining:");
+      Serial.println(10000 - (millis() - last_stop));
     }
   }
  
